a4a8448b7f907d3cc75d6d5c85c0444b5bcc6bbc
bizen63 <vlafarga@gmail.com>
2015-07-23 18:45:34 +0200

    Sensor de huella digital con Arduino.
    Sensor de huella digital para Arduino que muestra por el monitor serie
    nombre y apellidos de la persona con acceso permitido y enciende un led
    amarillo durante 3 segundos, en caso de acceso no permitidos muestra un
    mensaje por el monitor serie y enciende un led rojo durante 3 segundos.
    El proyecto incluye:
    
    - Librería con ejemplos de Adafruit.
    - Tutorial en pdf (teoría).
    - Proyecto (FingerprintAUTORIZA1).

---
+++ fingerprintAUTORIZA1/fingerprintAUTORIZA1.ino
@@ -0,0 +1,136 @@
+/*************************************************** 
+ 
+ ****************************************************/
+int verde = 6; // Conectar salida digital 6 a led verde
+int rojo = 7; // Conectar salida digital 7 a led rojo
+
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+int getFingerprintIDez();
+
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+SoftwareSerial mySerial(2, 3);
+
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  Serial.begin(9600);
+  Serial.println("fingertest");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  Serial.println("Waiting for valid finger...");
+  digitalWrite(verde, LOW);
+  digitalWrite(rojo, LOW);
+}
+
+void loop()                     // run over and over again
+{
+  getFingerprintIDez();
+  delay(50);            //don't ned to run this at full speed.
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  if (finger.fingerID == 1){
+  Serial.println(" D. LUIS FLORES ROJAS -ACCESO AUTORIZADO- "); 
+  digitalWrite(verde, HIGH);
+  delay(3000);
+  digitalWrite(verde, LOW);   
+  }
+  else if (finger.fingerID != 1){
+   Serial.println("  PERSONA NO AUTORIZADA ");
+  digitalWrite(rojo, HIGH);
+  delay(3000);
+  digitalWrite(rojo, LOW);   
+  }
+  return finger.fingerID; 
+}
